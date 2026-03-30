b99f287ada7db5c1839d623d89a92ee33c3ac316
e-seto <135999989+e-seto@users.noreply.github.com>
2026-03-30 16:15:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -11,27 +11,33 @@ app = Flask(
     static_folder=os.path.join(BASE_DIR, "static"),
 )
 
+# thresholds
+LEGIT_THRESHOLD = 0.60
+REVIEW_THRESHOLD = 0.85
+
 # routes
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/descriptive", methods=["GET"])
 def descriptive():
     return render_template("descriptive.html")
 
+
 @app.route("/diagnostic", methods=["GET"])
 def diagnostic():
     return render_template("diagnostic.html")
+
 
 @app.route("/predictive", methods=["GET"])
 def predictive():
     preditive_link = "https://project-wyfi.onrender.com/predictive"
     return redirect(preditive_link)
 
+
+
 @app.route("/prescriptive", methods=["GET"])
 def prescriptive():
     return render_template("prescriptive.html")
-
-#if __name__ == "__main__":
-#    app.run(debug=True)
